7dd787bbaed944d2499311bb12beac0fa0b03449
nathanstephenson <nathans261@gmail.com>
2023-03-04 23:10:49 +0000

    improved footer styling

--- portfolio/src/main/footer/Footer.tsx
+++ portfolio/src/main/footer/Footer.tsx
@@ -7,10 +7,12 @@ const FOOTER = "Footer"
 export default function Footer() {
 	return (
 	  <div id={FOOTER} className={FOOTER}>
-	  	<div>
+	  	<div className='Footer-row'>
 			Nathan Stephenson | <a href='https://github.com/nathanstephenson'  target='_blank' rel='noreferrer noopener'>{GITHUB_ICON} github</a>
 		</div>
-		<div>{'\uFF3B'}made with <a href='https://beta.reactjs.org/'  target='_blank' rel='noreferrer noopener'>React{'\uFF3D'}</a></div>
+		<div className='Footer-row'>
+			{'\uFF3B'}made with <a href='https://beta.reactjs.org/'  target='_blank' rel='noreferrer noopener'>React{'\uFF3D'}</a>
+		</div>
 	  </div>
 	);
   }
