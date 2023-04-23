c75e6553534ef0c4842f5a0ed5b571ed77b1bdb0
nathanstephenson <nathans261@gmail.com>
2023-04-23 07:14:34 +0100

    updated navbar to expand onclick, with new styling
    updated navbar to expand onclick, with new styling

--- src/main/navbar/Navbar.tsx
+++ src/main/navbar/Navbar.tsx
@@ -1,18 +1,14 @@
-import { useLayoutEffect, useState } from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import 'App.css'
 import './Navbar.css'
-import { getIntValue } from 'Utils'
 import { MOON, SUN, SQUARE as HOME, ABOUT, PROJECTS } from 'images/svg'
 
-const APP_INCLUDING_FOOTER = "App-including-footer"
 const BLUR_AMOUNT = "10px"
 
 const NAVBAR_ITEM = "NavbarItem"
 const NAVBAR = "Navbar"
-const NAVBAR_WIDTH_THIN = "5%"
-const NAVBAR_WIDTH_WIDE = "20%"
 
 const SMALL_HOME = HOME
 const WIDE_HOME = <>{SMALL_HOME} HOME</>
@@ -25,7 +21,9 @@ const NIGHT_LOGO = SUN
 
 export default function Navbar() {
 	var [pageState, setPageState] = useState({
-		currentPage: "",
+		currentPageAddress: "",
+		currentPageLogo: SMALL_HOME,
+		isWide: false,
 		darkMode: true
 	})
 	var [textState, setTextState] = useState({
@@ -34,28 +32,27 @@ export default function Navbar() {
 		projects: SMALL_PROJECTS,
 		dark: NIGHT_LOGO
 	})
-	const navbarWidthThin = NAVBAR_WIDTH_THIN//getNavbarWidthThin()
-
-	useLayoutEffect(() => {
-		setNavbarPosition()
-		window.addEventListener('resize', setNavbarPosition)
-	}, [])
 
 	function widen() {
-		if(isNavbarVertical()){
-			setTimeout(() => {
-				if(document.getElementById(NAVBAR)!.style.width === NAVBAR_WIDTH_WIDE){
-					setTextState({
-						home: WIDE_HOME,
-						about: WIDE_ABOUT,
-						projects: WIDE_PROJECTS,
-						dark: textState.dark
-					})
-				}
-			}, 50)
-			document.getElementById(NAVBAR)!.style.width = NAVBAR_WIDTH_WIDE
-			document.documentElement.style.setProperty("--main-blur-amount", BLUR_AMOUNT)
+		if(pageState.isWide){
+			return;
 		}
+		setTimeout(() => {
+			setTextState({
+				home: WIDE_HOME,
+				about: WIDE_ABOUT,
+				projects: WIDE_PROJECTS,
+				dark: textState.dark
+			})
+		}, 50)
+		setPageState({
+			currentPageAddress: pageState.currentPageAddress,
+			currentPageLogo: pageState.currentPageLogo,
+			isWide: true,
+			darkMode: pageState.darkMode
+		})
+		document.documentElement.style.setProperty("--main-blur-amount", BLUR_AMOUNT)
+		document.documentElement.style.setProperty("--body-interaction", "none")
 	}
 	
 	function shrink() {
@@ -65,21 +62,46 @@ export default function Navbar() {
 			projects: SMALL_PROJECTS,
 			dark: textState.dark
 		})
-		document.getElementById(NAVBAR)!.style.width = navbarWidthThin
+		setPageState({
+			currentPageAddress: pageState.currentPageAddress,
+			currentPageLogo: pageState.currentPageLogo,
+			isWide: false,
+			darkMode: pageState.darkMode
+		})
 		document.documentElement.style.setProperty("--main-blur-amount", "0px")
+		document.documentElement.style.setProperty("--body-interaction", "all")
 	}
 
-	const updateNightMode = () => {
-		const darkMode = !pageState.darkMode
+	function updateCurrentPage(linkTo: string, pageLogo: JSX.Element) {
+		if(linkTo === pageState.currentPageAddress){
+			setTimeout(shrink, 50)
+			return;
+		}
+		console.log(linkTo)
 		setPageState({
-			currentPage: pageState.currentPage,
-			darkMode: darkMode
+			currentPageAddress: linkTo,
+			currentPageLogo: pageLogo,
+			isWide: pageState.isWide,
+			darkMode: pageState.darkMode
 		})
+		console.log(pageState)
+		document.documentElement.scrollTop = 0;
+		setTimeout(shrink, 50)
+	}
+
+	function updateNightMode() {
+		const darkMode = !pageState.darkMode
 		setTextState({
 			home: textState.home,
 			about: textState.about,
 			projects: textState.projects,
 			dark: darkMode ? NIGHT_LOGO : DAY_LOGO
+		})
+		setPageState({
+			currentPageAddress: pageState.currentPageAddress,
+			currentPageLogo: pageState.currentPageLogo,
+			isWide: pageState.isWide,
+			darkMode: darkMode
 		})
 		if(darkMode){
 			document.documentElement.style.setProperty('--background-colour-primary', 'var(--background-colour-primary-dark)')
@@ -94,62 +116,30 @@ export default function Navbar() {
 		}
 	}
 
-	const updateCurrentPage = (linkTo: string) => {
-		setPageState({
-			currentPage: linkTo,
-			darkMode: pageState.darkMode
-		})
-		document.documentElement.scrollTop = 0;
-	}
-
 	return (
-		<div className={NAVBAR} id={NAVBAR} onMouseOver={widen} onMouseLeave={shrink}>
-			<NavbarItem name={textState.home} linkTo="" onClick={updateCurrentPage}/>
-			<NavbarItem name={textState.about} linkTo="about" onClick={updateCurrentPage}/>
-			<NavbarItem name={textState.projects} linkTo="projects" onClick={updateCurrentPage}/>
-			<NavbarItem name={textState.dark} linkTo={pageState.currentPage} onClick={updateNightMode}/>
+		<div className={NAVBAR} id={NAVBAR} onClick={widen} onMouseLeave={shrink}>
+			{pageState.isWide ? <>
+				<NavbarItem name={textState.home} linkTo="" pageLogo={SMALL_HOME} onClick={updateCurrentPage}/>
+				<NavbarItem name={textState.about} linkTo="about" pageLogo={SMALL_ABOUT} onClick={updateCurrentPage}/>
+				<NavbarItem name={textState.projects} linkTo="projects" pageLogo={SMALL_PROJECTS} onClick={updateCurrentPage}/>
+				<NavbarItem name={textState.dark} linkTo={pageState.currentPageAddress} pageLogo={pageState.currentPageLogo} onClick={updateNightMode}/>
+			</> : <>
+				<div id={Math.random().toString()} className={NAVBAR_ITEM}>{pageState.currentPageLogo}</div>
+			</>}
 		</div>
 	)
 }
 
-function NavbarItem(props: {name: JSX.Element, linkTo: string, onClick: (linkTo: string)=>void}) {
+function NavbarItem(props: {name: JSX.Element, linkTo: string, pageLogo: JSX.Element, onClick: (linkTo: string, pageLogo: JSX.Element)=>void}) {
 	return (
-		<div id={Math.random().toString()} className={NAVBAR_ITEM} onClick={()=>{props.onClick(props.linkTo)}}>
+		<div id={Math.random().toString()} className={NAVBAR_ITEM} onClick={()=>{props.onClick(props.linkTo, props.pageLogo)}}>
 			<Link to={props.linkTo}>{props.name}</Link>
 		</div>
 	)
 }
-function setNavbarPosition() {
-	const isVertical : boolean = isNavbarVertical()
-	const navbarWidthThin : string = getNavbarWidthThin()
-	document.documentElement.style.setProperty("--navbar-width-thin", navbarWidthThin)
-	document.documentElement.style.setProperty("--navbar-width-wide", NAVBAR_WIDTH_WIDE)
-	document.documentElement.style.setProperty("--navbar-item-spacing", "1" + (isVertical ? "vh" : "vw"))
-	document.getElementById(NAVBAR)!.style.bottom = isVertical ? "0vh" : "auto"
-	document.getElementById(NAVBAR)!.style.right = isVertical ? "auto" : "0vw"
-	document.getElementById(NAVBAR)!.style.flexDirection = isVertical ? "column" : "row"
-	document.getElementById(NAVBAR)!.style.height = isVertical ? "100%" : navbarWidthThin
-	document.getElementById(NAVBAR)!.style.minHeight = isVertical ? "100%" : navbarWidthThin
-	document.getElementById(NAVBAR)!.style.width = isVertical ? navbarWidthThin : "100%"
-	document.getElementById(NAVBAR)!.style.minWidth = isVertical ? navbarWidthThin : "100%"
-	// document.getElementById(APP_INCLUDING_FOOTER)!.style.marginLeft = isVertical ? navbarWidthThin : "0vw"
-	document.getElementById(APP_INCLUDING_FOOTER)!.style.marginTop = isVertical ? "0vh" : getIntValue(NAVBAR_WIDTH_THIN)+"%"
 
-	//Correctly position day/night option in navbar
-	const navbarItems = document.getElementsByClassName("NavbarItem")!
-	const lastNavbarItemId = navbarItems.item(navbarItems.length - 1)!.getAttribute("id")!
-	document.getElementById(lastNavbarItemId)!.style.marginInlineStart = isVertical ? "var(--navbar-item-spacing)" : "auto"
-	document.getElementById(lastNavbarItemId)!.style.marginBlockStart = isVertical ? "auto" : "var(--navbar-item-spacing)"
-}
-
-function getNavbarWidthThin(){
-	const amount = Math.min(getIntValue(NAVBAR_WIDTH_THIN) * getAspectRatio(), getIntValue(NAVBAR_WIDTH_THIN))
-	return amount + "%"
-}
-
-function isNavbarVertical() : boolean {
-	const aspect: number = getAspectRatio()
-	return aspect > 0.9
+function isPageVertical() : boolean {
+	return getAspectRatio() > 0.9
 }
 
 function getAspectRatio() : number {
